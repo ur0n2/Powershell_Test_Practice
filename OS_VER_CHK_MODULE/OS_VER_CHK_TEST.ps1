c59f8f595ddefe5590e2f4837dd8c17fd3ae88a8
ur0n2 <ur0n2@ur0n2.com>
2017-07-31 16:02:24 +0900

    Update OS Version Check module.

--- OS_VER_CHK_MODULE/OS_VER_CHK_TEST.ps1
+++ OS_VER_CHK_MODULE/OS_VER_CHK_TEST.ps1
@@ -1,3 +1,4 @@
-﻿Import-Module -Name ".\OS_VER_CHK.psm1" -Force
+﻿Remove-Module -Name OS_VER_CHK.psm1 -Force
+Import-Module -Name ".\OS_VER_CHK.psm1" -Force
 
 OS_VER_CHK
--- OS_VER_CHK_TEST.ps1
+++
@@ -1,3 +0,0 @@
-﻿Import-Module -Name ".\OS_VER_CHK.psm1" -Force
-
-OS_VER_CHK
