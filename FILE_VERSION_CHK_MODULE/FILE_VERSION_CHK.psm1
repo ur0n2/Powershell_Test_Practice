42fd53448ed85557dfb910feb2f1a253dcca7bb4
ur0n2 <ur0n2@ur0n2.com>
2017-07-31 15:52:15 +0900

    Update FILE_VERSION_CHK.ps1 and module

---
+++ FILE_VERSION_CHK_MODULE/FILE_VERSION_CHK.psm1
@@ -0,0 +1,16 @@
+﻿function FILE_VERSION_CHK
+{
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        $files,
+        [Parameter(Mandatory=$true)]
+        $files_path
+    )
+
+    foreach($file in $files) {
+        (Get-ChildItem -File -path $files_path -include $file -Recurse -EA SilentlyContinue) `
+        | foreach-object { " {0}`t {1}" -f $_.Name, `
+        [System.Diagnostics.FileVersionInfo]::GetVersionInfo($_).FileVersion }     
+    }
+}
---
+++ FILE_VERSION_CHK_MODULE/FILE_VERSION_CHK_TEST.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module -Name ".\FILE_VERSION_CHK.psm1" -Force
+
+Clear-Variable z_*
+$files = @("kernel32.dll", "ntdll.dll", "gdi32.dll", "user32.dll")
+$files_path = "C:\windows\system32\"
+
+FILE_VERSION_CHK $files $files_path 
